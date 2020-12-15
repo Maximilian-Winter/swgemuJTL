f95159b95de8a2a44839f6a5ae70397f2ab037c3
Maximilian Winter <maximilian.winter.91@gmail.com>
2020-12-15 18:42:22 +0100

    Updated AppearanceTemplate

--- MMOCoreORB/src/templates/appearance/AppearanceTemplate.h
+++ MMOCoreORB/src/templates/appearance/AppearanceTemplate.h
@@ -78,35 +78,33 @@ public:
 
 
 			Chunk *chunk = iffStream->openForm('HPTS');
-            try {
-                int numHardpoints = chunk->getChunksSize();
-                for (int i = 0; i < numHardpoints; i++) {
-                    iffStream->openChunk('HPNT');
-                    Matrix4 transform;
-                    transform[0][0] = iffStream->getFloat();
-                    transform[1][0] = iffStream->getFloat();
-                    transform[2][0] = iffStream->getFloat();
-                    transform[3][0] = iffStream->getFloat();
+            int numHardpoints = chunk->getChunksSize();
+			for (int i = 0; i < numHardpoints; i++) 
+			{
 
-                    transform[0][1] = iffStream->getFloat();
-                    transform[1][1] = iffStream->getFloat();
-                    transform[2][1] = iffStream->getFloat();
-                    transform[3][1] = iffStream->getFloat();
+				iffStream->openChunk('HPNT');
+				Matrix4 transform;
+				transform[0][0] = iffStream->getFloat();
+				transform[1][0] = iffStream->getFloat();
+				transform[2][0] = iffStream->getFloat();
+				transform[3][0] = iffStream->getFloat();
 
-                    transform[0][2] = iffStream->getFloat();
-                    transform[1][2] = iffStream->getFloat();
-                    transform[2][2] = iffStream->getFloat();
-                    transform[3][2] = iffStream->getFloat();
+				transform[0][1] = iffStream->getFloat();
+				transform[1][1] = iffStream->getFloat();
+				transform[2][1] = iffStream->getFloat();
+				transform[3][1] = iffStream->getFloat();
 
-                    String name;
-                    iffStream->getString(name);
-                    hardpoints.put(name, transform);
-					//Logger::console.info(fileName + " Loading hardpoint: " + name + " Transform: " + toString(transform), true);
-                    iffStream->closeChunk('HPNT');
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+				transform[0][2] = iffStream->getFloat();
+				transform[1][2] = iffStream->getFloat();
+				transform[2][2] = iffStream->getFloat();
+				transform[3][2] = iffStream->getFloat();
+
+				String name;
+				iffStream->getString(name);
+				hardpoints.put(name, transform);
+				//Logger::console.info(fileName + " Loading hardpoint: " + name + " Transform: " + toString(transform), true);
+				iffStream->closeChunk('HPNT');
+			}
 
 			// Skip loading hardpoints
 			iffStream->closeForm('HPTS');
